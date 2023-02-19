660f4dbc8f846bce7549588bc2fe63f691aacb40
Nakashima Sota <100900504+Sota-Nakashima@users.noreply.github.com>
2023-02-19 15:54:41 +0900

    2023/2/19 15:54

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+readonly SCRIPT_NAME=${0##*/}
+readonly VERSION=0.1.0
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+OUTPUT_DIR="./AESPA"
+
 # get conda init path
 ###################################
 conda_path=`which conda`
@@ -13,19 +19,46 @@ CONDA_INIT_PATH+="/etc/profile.d/conda.sh"
 
 ###################################
 
-while getopts :c:hV option
+print_version()
+{
+    cat << END
+AESPA build version $VERSION
+END
+}
+
+print_help()
+{
+    cat << END
+test version.
+END
+}
+
+print_short_help()
+{
+    cat << END
+test version.
+END
+}
+
+while getopts :o:c:t:hV option
 do
     case "$option" in
+        o)
+            OUTPUT_DIR=$OPTARG
+            ;;
+        t)
+            SRR_TABLE_PATH=$OPTARG
+            SET_SRR_TABLE_PATH=true
+            ;;
         c)
             CONDA_INIT_PATH=$OPTARG
-            echo $CONDA_INIT_PATH
             ;;
         h)
-            echo "help"
+            print_help
             exit 0
             ;;
         V)
-            echo "need"
+            print_version
             exit 0
             ;;
         :)
@@ -38,3 +71,23 @@ do
             ;;
     esac
 done
+
+if [[ "${SET_SRR_TABLE_PATH}" != "true" ]] ; then
+    printf '%s\n\n' "${SCRIPT_NAME}: need more option" 1>&2
+    print_short_help
+    exit 1
+fi
+
+if [[ ! -e $SRR_TABLE_PATH ]] ; then
+    printf '%s\n\n' "${SCRIPT_NAME}: options path is not exist." 1>&2
+    print_short_help
+    exit 1
+fi
+
+if [[ ! -e $CONDA_INIT_PATH ]] ; then
+    printf '%s\n\n' "${SCRIPT_NAME}: you have to check CONDA_INIT_PATH. please use -c option." 1>&2
+    print_short_help
+    exit 1
+fi
+
+echo "..build mode.."
